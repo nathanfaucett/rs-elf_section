30a0fd2a656176dba4f6a7c49803a42e2b880e91
nathanfaucett <nathanfaucett@gmail.com>
2016-04-27 09:14:58 -0500

    updates

--- src/lib.rs
+++ src/lib.rs
@@ -6,34 +6,6 @@ extern crate bitflags;
 
 
 #[derive(Debug)]
-#[repr(packed)] // repr(C) would add unwanted padding before first_section
-pub struct ElfSectionsTag {
-    typ: u32,
-    size: u32,
-    number_of_sections: u32,
-    entry_size: u32,
-    shndx: u32, // string table
-    first_section: ElfSection,
-}
-
-impl ElfSectionsTag {
-    pub fn get_type(&'static self) -> u32 { self.typ }
-    pub fn get_size(&'static self) -> u32 { self.size }
-    pub fn get_number_of_sections(&'static self) -> u32 { self.number_of_sections }
-    pub fn get_entry_size(&'static self) -> u32 { self.entry_size }
-    pub fn get_shndx(&'static self) -> u32 { self.shndx }
-    pub fn get_first_section(&'static self) -> &ElfSection { &self.first_section }
-
-    pub fn get_sections(&'static self) -> ElfSectionIter {
-        ElfSectionIter {
-            current_section: &self.first_section,
-            remaining_sections: self.number_of_sections - 1,
-            entry_size: self.entry_size,
-        }
-    }
-}
-
-#[derive(Debug)]
 #[repr(C)]
 pub struct ElfSection {
     name: u32,
